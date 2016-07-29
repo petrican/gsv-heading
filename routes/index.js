44d82f0f458d9348ea2ebc2c4babc603b6ca6238
Petrica Nanca <petrica_nanca@yahoo.com>
2016-07-29 10:37:52 +0300

    Service

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var panorama = require('google-panorama-by-location');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+   var location = [ req.query.lat, req.query.lng ]
+   panorama(location, function (err, result) {
+      if (!err && result) {
+          // Heading
+          res.json({ "success": true, "heading": result.camera_rotation.heading });
+      } else {
+          res.json({ "success": false, "error":"Could not get heading for given lat and long." });
+      }
+
+   });
+
 });
 
 module.exports = router;
